086f90e1e64acd07610e054d7b3e811b6f1f413b
jawadDouf <jdoufare2001@gmail.com>
2022-12-20 19:12:22 +0100

    create the entities of cme,cv and comment

--- src/main/java/com/cvtheque/cvtheque/models/Comment.java
+++ src/main/java/com/cvtheque/cvtheque/models/Comment.java
@@ -1,7 +1,20 @@
 package com.cvtheque.cvtheque.models;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Data;
 
 
+@Entity
+@Data
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(nullable = false)
+    private String body;
+
+    @ManyToOne
+    private Cme cme;
 }
--- src/main/java/com/cvtheque/cvtheque/models/Person.java
+++ src/main/java/com/cvtheque/cvtheque/models/Person.java
@@ -1,7 +1,26 @@
 package com.cvtheque.cvtheque.models;
 
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
+import lombok.Data;
 
 @MappedSuperclass
-public class Person {
+@Data
+public class Person{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(nullable = false)
+    private String first_name;
+
+    @Column(nullable = false)
+    private  String last_name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
 }
