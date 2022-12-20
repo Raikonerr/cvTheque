4b759d514ffc677d268c25c7ba0c22ad12359438
Elmehdi <93981356+Raikonerr@users.noreply.github.com>
2022-12-20 10:39:15 +0100

    Initial commit

---
+++ src/main/java/com/cvtheque/cvtheque/CvThequeApplication.java
@@ -0,0 +1,13 @@
+package com.cvtheque.cvtheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvThequeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvThequeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cvtheque/cvtheque/CvThequeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvtheque.cvtheque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvThequeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
