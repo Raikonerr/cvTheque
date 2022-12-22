ce2797be4f9a5225c8ed52e8283fe545c570ba53
Elmehdi <93981356+Raikonerr@users.noreply.github.com>
2022-12-22 11:57:21 +0100

    add : Learner's services

---
+++ src/main/java/com/cvtheque/cvtheque/services/LearnerServiceImp.java
@@ -0,0 +1,42 @@
+package com.cvtheque.cvtheque.services;
+
+import com.cvtheque.cvtheque.models.Learner;
+import com.cvtheque.cvtheque.repositories.LearnerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LearnerServiceImp implements LearnerService {
+   @Autowired
+    private LearnerRepo learnerRepo;
+
+    public LearnerServiceImp(LearnerRepo learnerRepo) {
+        this.learnerRepo = learnerRepo;
+    }
+
+    @Override
+    public Learner saveLearner(Learner learner) {
+        return learnerRepo.save(learner);
+    }
+
+    @Override
+    public Learner updateLearner(Learner learner) {
+        return learnerRepo.save(learner);
+    }
+
+    @Override
+    public void deleteLearner(Learner learner) {
+        learnerRepo.delete(learner);
+    }
+
+    @Override
+    public Learner getLearnerById(int id) {
+        return learnerRepo.findById(id).get();
+    }
+
+    @Override
+    public Iterable<Learner> getAllLearner() {
+        return learnerRepo.findAll();
+    }
+}
+
