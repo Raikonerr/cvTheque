5c067a981f1f846c0591f145b29880a9ba52437a
Elmehdi <93981356+Raikonerr@users.noreply.github.com>
2022-12-22 11:14:49 +0100

    add : interfaces  CV & learner & trainer repository

---
+++ src/main/java/com/cvtheque/cvtheque/repositories/CvRepo.java
@@ -0,0 +1,9 @@
+package com.cvtheque.cvtheque.repositories;
+
+import com.cvtheque.cvtheque.models.CV;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CvRepo extends JpaRepository<CV,Integer> {
+
+}
+
